c7e04c5c9459c86fc1677faa917862d3491b1d51
Tec <Tec@SKAB1-PC8>
2024-11-05 09:59:12 +0100

    Add project files.

---
+++ CountWord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Day1
+{
+    public class CountWord
+    {
+        public int WordCount(string text, string wordToCount)
+        {
+
+            if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(wordToCount))
+            {
+                return 0;
+            }
+            int count = 0;
+            int index = text.IndexOf(wordToCount, StringComparison.OrdinalIgnoreCase);
+
+            while (index != -1)
+            {
+                count++;
+                index = text.IndexOf(wordToCount, index + wordToCount.ToLower().Length, StringComparison.OrdinalIgnoreCase);
+            }
+
+            return count;
+        }
+    }
+
+}
